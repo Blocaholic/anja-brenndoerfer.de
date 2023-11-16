7aefce57d099d0d1b1c5691a474834f1420977da
Blocaholic <tobias@rocklands.de>
2023-11-16 17:56:19 +0100

    remove ad aside

--- www/index.php
+++ www/index.php
@@ -1,26 +1,23 @@
 <?php
 # Includes
-include_once('vars.php');
+include_once 'vars.php';
 # Welche Seite angefordert wird
-if(isset($_GET['page'])) {$get_page = $_GET['page'];}
-else{$get_page = 'home';}
+if (isset($_GET['page'])) {$get_page = $_GET['page'];} else { $get_page = 'home';}
 # Prüfen ob Inhaltsseite existiert, Fehlerseite anzeigen, falls nicht
-if(!file_exists("content/".$get_page.".htm")) {$get_page = 'notfound';}
+if (!file_exists("content/" . $get_page . ".htm")) {$get_page = 'notfound';}
 # Templates einlesen
 $html_content = file_get_contents($html_tpl);
 $navi_content = file_get_contents($navi_tpl[$get_page]);
 $ad_content = file_get_contents($ad_tpl[$get_page]);
-$content = "content/".$get_page.".htm";
+$content = "content/" . $get_page . ".htm";
 $content_content = file_get_contents($content);
 # Seite zusammensetzen / Platzhalter ersetzen
 $page = $html_content;
 $page = preg_replace("/\[\%navi\%\]/", $navi_content, $page);
 $page = preg_replace("/\[\%content\%\]/", $content_content, $page);
-$page = preg_replace("/\[\%ad\%\]/", $ad_content, $page);
 $timestamp = filemtime($content);
 $lastmod = date("D, j M Y H:i:s T", $timestamp);
 $page = preg_replace("/\[\%lastmod\%\]/", $lastmod, $page);
 $page = preg_replace("/\[\%title\%\]/", $title[$get_page], $page);
 # fertige Seite ausgeben
 echo $page;
-?>
